32d0c303c83afaa60c527b0464af834205a81c85
John Townsend <john.townsend@p28consulting.com>
2021-02-21 17:22:29 +0000

    Add amplify-infra-stack.ts and amplify.yml config files.

--- amplify-infra/lib/amplify-infra-stack.ts
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -1,9 +1,20 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as amplify from "@aws-cdk/aws-amplify";
 
 export class AmplifyInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Creation of the Amplify Application
+    const amplifyApp = new amplify.App(this, "react-app-github-amplify ", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "jtjiver",
+        repository: "amplify-react-test-repo",
+        oauthToken: cdk.SecretValue.secretsManager("aws-github-token", {
+          jsonField: "oauthToken",
+        }),
+      }),
+    });
+    const masterBranch = amplifyApp.addBranch("master");
   }
 }
